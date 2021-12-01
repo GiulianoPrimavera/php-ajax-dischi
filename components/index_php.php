fc060fbe0f22ab9725ad28d72568c60a72823e8a
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-01 17:50:17 +0100

    rename files

--- components/index_php.php
+++ components/index_php.php
@@ -30,6 +30,36 @@ $dischi = [
         "nomeArtista" => 'Eric Clapton',
         "annoDiPubblicazione" => "anno di pubblicazione",
     ],
+    [
+        "immagine" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",  
+        "nomeDisco" => 'New Jersey', 
+        "nomeArtista" => 'Bon Jovi',
+        "annoDiPubblicazione" => "anno di pubblicazione",
+    ],
+    [
+        "immagine" => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+        "nomeDisco" => "Ten's Summoner's Tales",
+        "nomeArtista" => 'Sting',
+        "annoDiPubblicazione" => "anno di pubblicazione",
+    ],
+    [
+        "immagine" => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+        "nomeDisco" => 'Steve Gadd Band', 
+        "nomeArtista" => 'Steve Gadd Band',
+        "annoDiPubblicazione" => "anno di pubblicazione",
+    ],
+    [
+        "immagine" => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+        "nomeDisco" => 'Brave new World',
+        "nomeArtista" => 'Iron Maiden',
+        "annoDiPubblicazione" => "anno di pubblicazione",
+    ],
+    [
+        "immagine" => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+        "nomeDisco" => 'One more car, one more raider', 
+        "nomeArtista" => 'Eric Clapton',
+        "annoDiPubblicazione" => "anno di pubblicazione",
+    ],
 ];
 ?>
 
@@ -43,7 +73,7 @@ $dischi = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/php_style.css">
     
 </head>
 <body>
